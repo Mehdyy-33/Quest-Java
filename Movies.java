44f2600ed79457b2fb85f931f2cade81a7dc70a7
mehdy guettache <mehdy33852@gmail.com>
2023-05-15 15:59:30 +0200

    second commit

--- Movies.java
+++ Movies.java
@@ -1,23 +1,17 @@
-import java.util.Arrays;
 
 public class Movies {
     public static void main(String[] args) {
-        String[] movie = { "Indiana Jones et le Royaume du Crâne de Cristal", "Indiana Jones et la Dernière Croisade",
-                "Indiana Jones et le Temple maudit" };
-        String[] actor1 = { "michel", "jean", "yve" };
-        String[] actor2 = { "julie", "julien", "christophe" };
-        String[] actor3 = { "maxime", "yann", "theo" };
-        String[][] movies;
 
-        movies = new String[][] {
-                movie,
-                actor1,
-                actor2,
-                actor3,
+        String[][] movies = {
+            { "Indiana Jones et le Royaume du Crâne de Cristal", "Indiana Jones et la Dernière Croisade",
+            "Indiana Jones et le Temple maudit" },
+            { "maxime", "yann", "theo","julie", "julien", "christophe","michel", "jean", "yve" }
         };
-        System.out.println(Arrays.toString(movies[0]));
-        System.out.println(Arrays.toString(movies[1]));
-        System.out.println(Arrays.toString(movies[2]));
+        for (int i = 0; i < movies[0].length; i++) {
+            String titles = movies[0][i];
+            String actors = movies[1][i];
+            System.out.println("Dans le film " + titles + ", les principaux acteurs sont : " + actors);
+        }
     }
 
 }
